107b187fffca4e3305fca965cb25ca35efaaabff
SWETHA S <125673867+SwethaSundar30@users.noreply.github.com>
2024-07-05 20:55:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function openmenu() {
+    document.getElementById("sidemenu").classList.add("open");
+}
+
+function closemenu() {
+    document.getElementById("sidemenu").classList.remove("open");
+}
